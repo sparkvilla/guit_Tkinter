6bb71cc4a907cabd9facec1b3e5f6891543437e5
sparkvilla <diego.villamaina@gmail.com>
2015-12-17 12:50:19 +0100

    Create gui_1.py

---
+++ gui_1.py
@@ -0,0 +1,22 @@
+import Tkinter as tk
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = tk.Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = tk.Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = tk.Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+root = tk.Tk() #
+my_gui = MyFirstGUI(root)
+root.mainloop()
